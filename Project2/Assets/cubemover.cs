89f77cb3cbf3b217b2a882b08b80616f4627f321
kumar <kumar.race@gmail.com>
2020-09-16 18:36:13 +0530

    Cube and Sphere

---
+++ Project2/Assets/cubemover.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class cubemover : MonoBehaviour
+{
+    // Update is called once per frame
+    
+    [SerializeField]
+    private float speed = 1;
+
+    void Update()
+    {
+
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontal,vertical);
+
+        transform.position += movement * Time.deltaTime * speed; 
+    }
+   
+    public void OnCollisionEnter(Collision collision){
+        SceneManager.LoadScene(1);
+    } 
+}
+
---
+++ Project2/Assets/respawn.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class respawn : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.y<=-10){
+            Respawn();
+        }
+    }
+    
+    void Respawn(){
+        float randomX = UnityEngine.Random.Range(-10,10);
+        float randomY = UnityEngine.Random.Range(10,15);
+        transform.position = new Vector3(randomX,randomY);
+        var rigidBody = GetComponent<Rigidbody>();
+        rigidBody.velocity = Vector3.zero;
+    }
+    
+    private void OnEnable(){
+        Respawn();
+    }
+}
